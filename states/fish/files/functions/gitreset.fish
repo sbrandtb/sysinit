3e7c7ba797db98236b79f85b171f7dcafd841df3
Sebastian Brandt <sebastian.brandt@friday.de>
2017-08-24 13:48:28 +0200

    Update gitreset.fish

--- states/fish/files/functions/gitreset.fish
+++ states/fish/files/functions/gitreset.fish
@@ -1,9 +1,12 @@
-function gitreset --description 'set local branch to updated master, prune remote and local branches.'
+function gitreset --description 'set local branch to updated BRANCH (default: master), prune remote and local branches.' --argument BRANCH
+    if test -z "$BRANCH"
+        set BRANCH master
+    end
     if not git stash | grep -q "No local changes to save"
         set stashed "1"
     end
-    git checkout master
-    git pull --rebase ;and git remote prune origin ;and git branch --merged | grep -v "\*" | xargs -r -n 1 git branch -d
+    git checkout $BRANCH
+    git pull --rebase ;and git remote prune origin ;and git branch --merged $BRANCH | grep -v "\*" | xargs -r -n 1 git branch -d
     and if test -n "$stashed"
        git stash pop
     end
